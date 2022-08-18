39977a02ea2aa51bfabf3c32ba2d4a85ad94ba14
debora <debora.r.serra@gmail.com>
2022-08-18 12:53:31 -0300

    test: finish fixing tests

--- app/backend/src/service/User.service.ts
+++ app/backend/src/service/User.service.ts
@@ -3,13 +3,13 @@ import User from '../database/models/User.model';
 
 class UserService {
   static async getOne(email: string) {
-    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/g;
     if (!regex.test(email)) {
       throw new ErrorCode('Incorrect email or password', 401);
     }
     const response = await User.findOne({ where: { email }, raw: true });
     if (!response) {
-      throw new ErrorCode('Incorrect email or password', 400);
+      throw new ErrorCode('Incorrect email or password', 401);
     }
     return response;
   }
